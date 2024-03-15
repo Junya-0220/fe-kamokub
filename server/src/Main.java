71ab0ccd15de4d118422044c74e9b830ca760bd7
Junya-0220 <junya010220@gmail.com>
2024-03-15 23:46:37 +0900

    配列

--- server/src/Main.java
+++ server/src/Main.java
@@ -2,36 +2,21 @@ import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        int month = calendar.get(Calendar.MONTH) + 1;
+        int value;
+        int[][] array = new int[][]{
+            {3,0,0,0,0},
+            {0,2,2,0,0},
+            {0,0,0,1,3},
+            {0,0,0,2,0},
+            {0,0,0,0,1},
+        };
+        int right,left;
+        int temp;
 
-        String month_name = "";
-
-        if(month == 1){
-            System.out.println("1月");
-        }else if(month == 2){
-            System.out.println("2月");
-        }else if(month == 3){
-            System.out.println("3月");
-        }else if(month == 4){
-            System.out.println("4月");
-        }else if(month == 5){
-            System.out.println("5月");
-        }else if(month == 6){
-            System.out.println("6月");
-        }else if(month == 7){
-            System.out.println("7月");
-        }else if(month == 8){
-            System.out.println("8月");
-        }else if(month == 9){
-            System.out.println("9月");
-        }else if(month == 10){
-            System.out.println("10月");
-        }else if(month == 11){
-            System.out.println("11月");
-        }else if(month == 12){
-            System.out.println("12月");
-        }
-
+        System.out.println(array[0][0]);
+        System.out.println(array[1][0]);
+        System.out.println(array[2][0]);
+        System.out.println(array[3][0]);
+        System.out.println(array[4][0]);
     }
 }
