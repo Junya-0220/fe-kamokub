6809b16a4b77f2469b0f505b6d78727affbd9a17
Junya-0220 <junya010220@gmail.com>
2024-03-17 17:05:13 +0900

    演習３つ　for,配列の平均,バブルソート

--- server/src/Main.java
+++ server/src/Main.java
@@ -2,19 +2,62 @@ import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = new int[]{9, 10, 0, 2, 87, 1000};
-        for(int i = 0; i <= a.length - 2; i++){
-           for(int j = a.length - 1; j >= i +1; j--) {
-                if(a[j] < a[j - 1]){
-                    int w = a[j];
-                    a[j] = a[j - 1];
-                    a[j - 1] = w;
+        /* for文とwhile文
+        int[] array = new int[]{98,12,9,102,88};
+        for(int i = 0; i <= array.length -1; i++){
+            System.out.println(array[i]);
+        }
+
+        for(int value: array){
+            System.out.println(value);
+        }
+         */
+
+        /* 配列の平均を出す
+        int[] array = new int[]{98,12,9,102,88};
+        int i = 0;
+        int result = 0;
+        while(i <= array.length -1){
+            result = result + array[i];
+            i++;
+        }
+        System.out.println(result/array.length);
+
+         */
+        
+        /*バブルソート
+         int[]A = new int[]{100,0,15,22,69,985,47};
+
+         for(int i = 0; i <= A.length-2; i++){
+            for(int j = A.length - 1; j >= i + 1; j--){
+                if(A[j] < A[j -1]){
+                    int w = A[j];
+                    A[j] = A[j-1];
+                    A[j - 1] = w;
                 }
             }
         }
-        // ソートできたか確認
-        for(int i = 0; i <= a.length -1; i++){
-            System.out.println(a[i]);
+
+        for(int i = 0; i <= A.length -1; i++){
+            System.out.println(A[i]);
         }
+         */
+
+         int[]A = new int[]{100,0,15,22,69,985,47};
+
+         for(int i = 0; i <= A.length-2; i++){
+            for(int j = A.length - 1; j >= i + 1; j--){
+                if(A[j] < A[j -1]){
+                    int w = A[j];
+                    A[j] = A[j-1];
+                    A[j - 1] = w;
+                }
+            }
+        }
+
+        for(int i = 0; i <= A.length -1; i++){
+            System.out.println(A[i]);
+        }
+
     }
 }
