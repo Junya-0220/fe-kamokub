285b89a2e1ea214cf26dd82af9995fbe4e0ce661
Junya-0220 <junya010220@gmail.com>
2024-03-31 21:35:30 +0900

    平方根の関数

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,63 +1,10 @@
-import java.util.Calendar;
-
 public class Main {
     public static void main(String[] args) {
-        /* for文とwhile文
-        int[] array = new int[]{98,12,9,102,88};
-        for(int i = 0; i <= array.length -1; i++){
-            System.out.println(array[i]);
-        }
 
-        for(int value: array){
-            System.out.println(value);
-        }
-         */
+        System.out.println(calc(32, 12));
+    }
 
-        /* 配列の平均を出す
-        int[] array = new int[]{98,12,9,102,88};
-        int i = 0;
-        int result = 0;
-        while(i <= array.length -1){
-            result = result + array[i];
-            i++;
-        }
-        System.out.println(result/array.length);
-
-         */
-        
-        /*バブルソート
-         int[]A = new int[]{100,0,15,22,69,985,47};
-
-         for(int i = 0; i <= A.length-2; i++){
-            for(int j = A.length - 1; j >= i + 1; j--){
-                if(A[j] < A[j -1]){
-                    int w = A[j];
-                    A[j] = A[j-1];
-                    A[j - 1] = w;
-                }
-            }
-        }
-
-        for(int i = 0; i <= A.length -1; i++){
-            System.out.println(A[i]);
-        }
-         */
-
-         int[]A = new int[]{100,0,15,22,69,985,47};
-
-         for(int i = 0; i <= A.length-2; i++){
-            for(int j = A.length - 1; j >= i + 1; j--){
-                if(A[j] < A[j -1]){
-                    int w = A[j];
-                    A[j] = A[j-1];
-                    A[j - 1] = w;
-                }
-            }
-        }
-
-        for(int i = 0; i <= A.length -1; i++){
-            System.out.println(A[i]);
-        }
-
+    private static double calc(double x, double y) {
+        return Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
     }
 }
