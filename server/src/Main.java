7ef3b5f92f08f651f665dc3911c47614a82ece98
Junya-0220 <junya010220@gmail.com>
2024-03-31 22:24:01 +0900

    ビット演算

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,10 +1,33 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(calc(32, 12));
+        System.out.println(Integer.toBinaryString(rev(0b01001011)));
     }
+    /*
+    rbyte 01001011
+    01001011
+    00000001
+    欲しいところを1にして論理積を取る
+    
+    step1
+    00000000 or
+    00000001
+    00000001
 
-    private static double calc(double x, double y) {
-        return Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
+    r = 00000001
+
+    rbyte 00100101
+     */
+    private static int rev(int _byte){
+        int rbyte = _byte;
+        int r = 0b00000000;
+        int i;
+
+        for(i=1;i<=8;i++){
+            r = (r << 1) | (rbyte & 0b00000001);
+            rbyte = rbyte >> 1;
+        }
+
+        return r;
     }
 }
